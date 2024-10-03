adc17d8870feb813ce93cbb7fb6f7de3c64bc829
AnishRM-ai <anishmagar228@gmail.com>
2024-10-03 21:37:52 +0545

    request header parser microservices built for fcc certification.

---
+++ index.js
@@ -0,0 +1,34 @@
+// index.js
+// where your node app starts
+
+// init project
+require('dotenv').config();
+var express = require('express');
+var app = express();
+
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+
+app.use(express.static('public'));
+
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/api/whoami', (req, res)=>{
+  const ipaddress = req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
+  res.json({
+    ipaddress: ipaddress,
+    language: language,
+    software:software,
+  });
+});
+// listen for requests :)
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
